16735b5bf9a884ba00d72a96fd3812bf339532c2
Rebotta Yolo <125689322+R3b0tta@users.noreply.github.com>
2025-03-21 18:41:22 +0300

    feat: feated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import { Home } from "./pages/Home";
 import { FormPage } from "./pages/FormPage";
-import "./scss/app.scss"
+import "./scss/app.scss";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/form" element={<FormPage />} />
+        <Route path="/connect-hub" element={<Home />} />
+        <Route path="/connect-hub/form" element={<FormPage />} />
       </Routes>
     </div>
   );
